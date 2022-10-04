28360a2cd2cbe10b434c417adaf8e753893d168c
priyadharshiniashok07 <114928257+priyadharshiniashok07@users.noreply.github.com>
2022-10-04 20:05:37 +0530

    Update Prime1.java
    updated

--- Prime1.java
+++ Prime1.java
@@ -2,19 +2,20 @@ package prime;
 
 public class Prime1 {
     public static void main(String[] args) {
-        int n=20;
-        for (int i=2;i<=n;i++){
-            int count=0;
-            for (int j=2;j<i;j++){
-                if(i%j==0){
-                    count++;
-
-                }
-            }
-            if(count==0){
-                System.out.println(i+" ");
+        
+         int num = 29;
+        boolean str = false;
+        for (int i = 2; i <= num / 2; ++i) {
+            // condition for nonprime number
+            if (num % i == 0) {
+                str = true;
+                break;
             }
         }
 
+        if (!str)
+            System.out.println(num + " is a prime number.");
+        else
+            System.out.println(num + " is not a prime number.");
     }
 }
